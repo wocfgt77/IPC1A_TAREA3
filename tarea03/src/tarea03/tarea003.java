54608aade1dbb27a00e2126906b5f3b5ff9d0cff
wocfgt77 <wocf-gt@hotmail.com>
2015-03-02 22:09:48 -0600

    TAREA3 PROGRAMA

---
+++ tarea03/src/tarea03/tarea003.java
@@ -0,0 +1,26 @@
+
+//199911764
+package tarea03;
+
+
+import java.util.Scanner;   
+
+public class tarea003 {  
+ 
+	public static void main(String[] args) {  
+      
+	   Scanner s=new Scanner(System.in);  
+       
+	   System.out.println("INGRESE NUMERO ");  
+        int a=s.nextInt();  
+       
+        if(a%2==0){  
+             System.out.println(" ES PAR");  
+        
+        }else{  
+            System.out.println(" ES IMPAR");  
+        }        
+     }  
+
+	}
+
